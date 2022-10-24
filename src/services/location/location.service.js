bde95bac206b5b301b0f0facadd04e3013a6bd17
Wilmer <herrerawilmer.94@gmail.com>
2022-10-24 14:37:59 -0600

    functions

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,17 +1,17 @@
+/* eslint-disable comma-dangle */
 import camelize from "camelize";
-import { locations } from "./location.mock";
 
 export const locationRequest = (searchTerm) => {
-  return new Promise((resolve, reject) => {
-    const locationMock = locations[searchTerm];
-    if (!locationMock) {
-      reject("not found");
-    }
-    resolve(locationMock);
+  return fetch(
+    `http://127.0.0.1:5001/mealstogo-ca4f2/us-central1/geocode?city=${searchTerm}`
+  ).then((res) => {
+    return res.json();
   });
 };
 
 export const locationTransform = (result) => {
+  console.log(result);
+
   const formattedResponse = camelize(result);
   const { geometry = {} } = formattedResponse.results[0];
   const { lat, lng } = geometry.location;
